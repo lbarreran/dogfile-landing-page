100924bec180923df312e3cc52c9fcdb42c66270
Lorena Barrera <l.barreranicolas@gmail.com>
2024-03-31 18:40:37 +0200

    first changes

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'React landing page',
-  title: 'React landing page template 2021',
+  title: 'Una app para gestionar el cuidado completo de tu perro',
   description: 'Production ready plug n play landing page!',
   locale: 'en',
 };
